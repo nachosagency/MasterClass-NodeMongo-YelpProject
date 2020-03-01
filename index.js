6f125331e32736dd667290c7dd74592f97db4008
Gilbert NZEKA <nzeka@technachos.com>
2020-03-01 16:49:14 +0100

    Intégrer live debugging comme sur Angular

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var app = express();
 app.engine(".html", require("ejs").__express);
 app.set("views", __dirname + "/views");
 app.set("view engine", "html");
+app.use(express.static('public'));
+
 var templateVersion = 'v1';
 
 app.get("/", function(req, res){
