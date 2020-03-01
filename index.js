14ab2e78b4b355f7f49b27cc8de106a4127526d1
Gilbert NZEKA <nzeka@technachos.com>
2020-03-01 17:07:52 +0100

    Récupérer champs de recherche

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ console.log("Launching app...");
 
 var express = require("express");
 var fs = require("fs");
+var request = require('request');
 
-var app = express();
-app.engine(".html", require("ejs").__express);
-app.set("views", __dirname + "/views");
-app.set("view engine", "html");
-app.use(express.static('public'));
+var app = express(); 
+app.engine(".html", require("ejs").__express); // Configuration Express
+app.set("views", __dirname + "/views"); // Configuration Express
+app.set("view engine", "html"); // Configuration Express
+app.use(express.static('public')); // Ajout d'un dossier statique pour les images et CSS 
+app.use(express.urlencoded({ extended: true })) // Analyser les requêtes d'URLs encodés (tels que envoyés par les formulaires HTML)
+app.use(express.json()); // Analyser les requêtes JSON (envoyés par les clients d'API)
 
 var templateVersion = 'v1';
 
@@ -19,12 +22,21 @@ app.get("/", function(req, res){
     });
 });
 
-app.get("/search", function(req, res){
-
-    res.render(templateVersion+'_index', {
-        searchTag: '',
-        searchAddress: '',
-        bizFromApi: []
+app.post("/search", function(req, res){
+    request('http://127.0.0.1:8080/chinese+food+bordeaux.json', { json: true }, (err, res2, body) => {
+        var _bizFromApi = [];
+        if (err) { 
+            console.log(err); 
+            _bizFromApi = null;
+        }else{
+            _bizFromApi = ( body.businesses ) ? body.businesses : null;
+        }
+        
+        res.render(templateVersion+'_index', {
+            searchTag: ( req.body.search_term ) ? req.body.search_term : '',
+            searchAddress: ( req.body.address ) ? req.body.address : '',
+            bizFromApi: _bizFromApi
+        });
     });
 });
 
